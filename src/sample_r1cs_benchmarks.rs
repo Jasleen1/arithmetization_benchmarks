38fc2b7e98490aa6ab3381a48cee731fb2248d00
Tom Yurek <tomyurek@gmail.com>
2023-04-25 13:42:03 -0400

    update example to fit new api

--- src/sample_r1cs_benchmarks.rs
+++ src/sample_r1cs_benchmarks.rs
@@ -145,21 +145,18 @@ pub(crate) fn orchestrate_r1cs_example<
     let degree_fs = r1cs.num_cols();
     println!("---------------------");
     println!("Step 3: Building Index Domains");
-    let index_domains = build_index_domains::<B, E>(index_params.clone());
+    let index_domains = build_index_domains::<B>(index_params.clone());
     println!("built index domains");
     println!("---------------------");
     println!("Step 3: Building Indexes");
     let now = Instant::now();
-    let indexed_a = index_matrix::<B, E>(&r1cs.A, &index_domains);
-    r1cs.A = Matrix::new("dummy A", Vec::<Vec<B>>::new()).unwrap();
+    let indexed_a = index_matrix::<B>(&mut r1cs.A, &index_domains);
     println!("Indexed A in {} ms", now.elapsed().as_millis());
     let now = Instant::now();
-    let indexed_b = index_matrix::<B, E>(&r1cs.B, &index_domains);
-    r1cs.B = Matrix::new("dummy B", Vec::<Vec<B>>::new()).unwrap();
+    let indexed_b = index_matrix::<B>(&mut r1cs.B, &index_domains);
     println!("Indexed B in {} ms", now.elapsed().as_millis());
     let now = Instant::now();
-    let indexed_c = index_matrix::<B, E>(&r1cs.C, &index_domains);
-    r1cs.C = Matrix::new("dummy C", Vec::<Vec<B>>::new()).unwrap();
+    let indexed_c = index_matrix::<B>(&mut r1cs.C, &index_domains);
     println!("Indexed C in {} ms", now.elapsed().as_millis());
     // This is the index i.e. the pre-processed data for this r1cs
     let index = Index::new(index_params.clone(), indexed_a, indexed_b, indexed_c);
